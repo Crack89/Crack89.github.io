95a3e8b9ea478a6711b77f779f78bea13616db13
Crack89 <36825150+Crack89@users.noreply.github.com>
2018-04-13 10:13:55 +0200

    bhjbj

--- style.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementsByClassName("menu-icon")[0];
-var menu = document.getElementsByClassName("menu")[0];
-button.onclick = function() {
-    menu.classList.toggle("show");
-    return false;
-}
