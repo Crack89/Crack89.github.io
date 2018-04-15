e981dce6fb3f67ec111102d53a93fc1ef491d482
Crack89 <36825150+Crack89@users.noreply.github.com>
2018-04-15 21:44:47 +0200

    hfjkf

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ button.onclick = function() {
     menu.classList.toggle("show");
     return false;
 }
+
+
+var marginY = 0;
+var destination = 0;
+var speed = 9;
+var scroller = null;
+
+function initScroll(elementId){
+    destination = document.getElementById(elementId).offsetTop;
+    // console.log(destination);
+    scroller = setTimeout(function(){
+        initScroll(elementId);
+    }, 1);
+
+    marginY = marginY + speed;
+
+    if(marginY >=destination){
+        clearTimeout(scroller);
+    }
+
+    window.scroll(0, marginY);
+}
+function toTop(){
+    scroller = setTimeout(function(){
+        toTop();
+    }, 1);
+
+    marginY = marginY - speed;
+
+    if(marginY <= 0){
+        clearTimeout(scroller);
+    }
+
+    window.scroll(0, marginY);
+}
